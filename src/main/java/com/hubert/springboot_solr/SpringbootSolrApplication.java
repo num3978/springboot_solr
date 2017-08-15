79b7a1605549f4efa22303f6d31bff2ccc433940
zhengxiaobin <xiaobin.zheng@hand-china.com>
2017-08-15 16:02:14 +0800

    first commit

---
+++ src/main/java/com/hubert/springboot_solr/SpringbootSolrApplication.java
@@ -0,0 +1,12 @@
+package com.hubert.springboot_solr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSolrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSolrApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hubert/springboot_solr/SpringbootSolrApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hubert.springboot_solr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootSolrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
